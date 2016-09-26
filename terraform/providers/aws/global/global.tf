d7e3610c4eb59e6a3a4d84893266a5d7a2642857
Nathaniel Thurston <njt@ipvive.com>
2016-09-26 16:56:20 -0700

    remove global static website.

--- terraform/providers/aws/global/global.tf
+++ terraform/providers/aws/global/global.tf
@@ -70,22 +70,6 @@ resource "aws_route53_zone" "zone" {
   name = "${var.domain}"
 }
 
-module "prod_website" {
-  source = "../../../modules/aws/util/website"
-
-  route_zone_id = "${aws_route53_zone.zone.zone_id}"
-  fqdn          = "${var.domain}"
-  sub_domain    = "${var.domain}"
-}
-
-module "staging_website" {
-  source = "../../../modules/aws/util/website"
-
-  route_zone_id = "${aws_route53_zone.zone.zone_id}"
-  fqdn          = "staging.${var.domain}"
-  sub_domain    = "staging"
-}
-
 output "config" {
   value = <<CONFIG
 
@@ -114,14 +98,5 @@ output "iam_admin_secret_keys" { value = "${module.iam_admin.secret_keys}" }
 output "iam_vault_users"       { value = "${module.iam_vault.users}" }
 output "iam_vault_access_ids"  { value = "${module.iam_vault.access_ids}" }
 output "iam_vault_secret_keys" { value = "${module.iam_vault.secret_keys}" }
-
-output "prod_domain"      { value = "${module.prod_website.domain}" }
-output "prod_endpoint"    { value = "${module.prod_website.endpoint}" }
-output "prod_fqdn"        { value = "${module.prod_website.fqdn}" }
-output "prod_zone_id"     { value = "${module.prod_website.hosted_zone_id}" }
-output "staging_domain"   { value = "${module.staging_website.domain}" }
-output "staging_endpoint" { value = "${module.staging_website.endpoint}" }
-output "staging_fqdn"     { value = "${module.staging_website.fqdn}" }
-output "staging_zone_id"  { value = "${module.staging_website.hosted_zone_id}" }
 
 output "zone_id" { value = "${aws_route53_zone.zone.zone_id}" }
